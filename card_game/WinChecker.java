aa4f771d50372097e2c470d96d3209807948294c
Euan Fullarton <euanfullarton7@gmail.com>
2017-04-14 13:59:26 +0100

    moved pseudocode from WinCheckerTest to WinChecker

--- card_game/WinChecker.java
+++ card_game/WinChecker.java
@@ -6,4 +6,26 @@ import java.util.*;
 public class WinChecker {
 
 
+    // highest card(){
+    // if player1.value_of_card > player2.value_of_card
+    //   player1 wins
+    // elsif player2.value_of_card > player1.value_of_card
+    //   player2 wins
+    // else 
+    //   draw
+    // end
+    // }
+
+
+  // highest hand(){
+  // if player1.value_of_hand > player2.value_of_hand
+  //   player1 wins
+  // elsif player2.value_of_hand > player1.value_of_hand
+  //   player2 wins
+  // else 
+  //   draw
+  // end
+  // }
+
+
 }
--- card_tests/WinCheckerTest.java
+++ card_tests/WinCheckerTest.java
@@ -6,25 +6,4 @@ import java.util.*;
 
 public class WinCheckerTest {
 
-  // highest card(){
-  // if player1.value_of_card > player2.value_of_card
-  //   player1 wins
-  // elsif player2.value_of_card > player1.value_of_card
-  //   player2 wins
-  // else 
-  //   draw
-  // end
-  // }
-
-
-// highest hand(){
-// if player1.value_of_hand > player2.value_of_hand
-//   player1 wins
-// elsif player2.value_of_hand > player1.value_of_hand
-//   player2 wins
-// else 
-//   draw
-// end
-// }
-
 }
